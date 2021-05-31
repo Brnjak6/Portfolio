4184121b4fc5193c76b2b5df4c13c3415abb084f
Brnjak6 <68736438+Brnjak6@users.noreply.github.com>
2021-05-31 14:47:59 +0200

    Starter

---
+++ components/Navbar.js
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="navbar">Hello</div>;
+};
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import LandingPage from "../components/LandingPage";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Stjepan | Portfolio</title>
+      </Head>
+      <LandingPage />
+    </>
+  );
+}
