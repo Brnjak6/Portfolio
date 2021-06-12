8513aafef267da4c0c0166ac97c35d5509c26f7c
Stjepan Brnjak <stjepan136@gmail.com>
2021-06-12 14:29:52 +0200

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,21 @@ import Layout from "../components/Layout";
 import Head from "next/head";
 import "../styles/Main.scss";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../library/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
